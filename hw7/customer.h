b41d1664457e304e15c9c8ecb938dbdd2472bc7a
Ricardo Ramirez <rramir76@ucsc.edu>
2025-03-15 01:32:08 +0000

    Adding final customer.h file

---
+++ hw7/customer.h
@@ -0,0 +1,16 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+typedef struct Customer {
+  char *email;
+  char *name;
+  int shoe_size;
+  char *favorite_food;
+  struct Customer *next;
+} Customer;
+
+Customer *create_customer(const char *email, const char *name, int shoe_size,
+                          const char *favorite_food);
+void free_customer(Customer *customer);
+
+#endif
